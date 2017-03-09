ef9e3f6a7c9befc193f0862e77c2d1695b5415d3
Torbjørn Viem Ness <torbjorn@viem-it.no>
2017-03-09 11:19:27 +0100

    Added some comments about GPIO

--- RPi/src/sensor.py
+++ RPi/src/sensor.py
@@ -1,4 +1,15 @@
 from mqtt_client import QOS_0, QOS_1, QOS_2
+# We could either use https://sourceforge.net/p/raspberry-gpio-python/wiki/Home/
+# Or just use the raspbian-included binary /usr/bin/gpio
+'''
+Example: 
+def gpio(cmd, pin, value):
+	return subprocess.check_output(["gpio", cmd, str(pin), value])
 
+# Read
+gpio("read", pin, None)
 
-
+# Write
+gpio("write", pin, value)
+'''
+# The disadvantage of calling the binary is that you don't get to subscribe to events on the pins, but have to poll (not really that big a problem though)
