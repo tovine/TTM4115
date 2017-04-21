e2a867a7acf592fb432db59f5d603994e3a20e90
Peder Bergebakken Sundt <pbsds@hotmail.com>
2017-04-21 10:54:30 +0200

    sanitize sql parameters

--- webserver/database.py
+++ webserver/database.py
@@ -19,7 +19,7 @@ async def close_pg(app):
 	await app['pool'].wait_closed()	
 
 #execute a direct query
-async def execute(request, query, fetch=False):
+async def execute(request, query, fetch=False, params=None):
 	if ";" in query:
 		raise Exception("Multible queries caught: %s" % query)
 	
@@ -27,7 +27,9 @@ async def execute(request, query, fetch=False):
 	
 	async with app["pool"].acquire() as conn:
 		async with conn.cursor() as cur:
-			await cur.execute(query)
+			#q = await cur.mogrify(query, params)
+			#print(q)
+			await cur.execute(query, params)
 			if fetch:
 				if type(fetch) is int:
 					ret = await cur.fetchmany(fetch)
@@ -38,50 +40,49 @@ async def execute(request, query, fetch=False):
 
 #premade queries
 async def select_tags(request, fetch=True):#(id, name)
-	ret = await execute(request, f"SELECT * FROM tags", fetch)
+	ret = await execute(request, "SELECT * FROM tags", fetch)
 	return ret
 
 async def select_toilets(request, fetch=True):#(id, lat, lng, name, poi_id)
-	ret = await execute(request, f"SELECT * FROM toilets ORDER BY id DESC", fetch)
+	ret = await execute(request, "SELECT * FROM toilets ORDER BY id DESC", fetch)
 	return ret
 
 async def select_toilet(request, toiletid):#(id, lat, lng, name, poi_id)
-	ret = await execute(request, f"SELECT * FROM toilets WHERE id={toiletid}", 1)
+	ret = await execute(request, f"SELECT * FROM toilets WHERE id={int(toiletid)}", 1)
 	return ret
 
 async def select_toilet_statuses(request, fetch=True):#(id, lat, lng, name, status, dt)
-	ret = await execute(request, f"SELECT * FROM toilet_status", fetch)
+	ret = await execute(request, "SELECT * FROM toilet_status", fetch)
 	return ret
 
 async def select_toilet_status(request, toiletid):#(id, lat, lng, name, status, dt)
-	ret = await execute(request, f"SELECT * FROM toilet_status WHERE id={toiletid}", 1)
+	ret = await execute(request, f"SELECT * FROM toilet_status WHERE id={int(toiletid)}", 1)
 	return ret
 
 async def select_toilets_by_tagnames(request, tagnames, fetch=True):#todo:test
-	tags = ", ".join(map(repr, tagnames))
-	ret = await execute(request,f"SELECT * FROM toilets as s WHERE s.id IN (SELECT t.toilet FROM toilet_tags as t JOIN tags ON tags.id = t.tag WHERE tags.name in ({tags}))", fetch)
+	tagnames = tuple(map(str, tags))
+	ret = await execute(request,"SELECT * FROM toilets as s WHERE s.id IN (SELECT t.toilet FROM toilet_tags as t JOIN tags ON tags.id = t.tag WHERE tags.name IN %s)", fetch, params=(tagnames,))
 	return ret
 	
 async def select_toilet_statuses_by_tagnames(request, tagnames, fetch=True):#todo:test
-	tags = ", ".join(map(repr, tagnames))
-	ret = await execute(request,f"SELECT * FROM toilet_status as s WHERE s.id IN (SELECT t.toilet FROM toilet_tags as t JOIN tags ON tags.id = t.tag WHERE tags.name in ({tags}))", fetch)
+	tagnames = tuple(map(int, tags))
+	ret = await execute(request,"SELECT * FROM toilet_status as s WHERE s.id IN (SELECT t.toilet FROM toilet_tags as t JOIN tags ON tags.id = t.tag WHERE tags.name IN %s)", fetch, params=(tagnames,))
 	return ret
 	
 async def select_toilet_statuses_by_tags(request, tags, fetch=True):#todo:test
-	tags = ", ".join(map(str, tags))
-	ret = await execute(request,f"SELECT * FROM toilet_status as s WHERE s.id IN (SELECT t.toilet FROM toilet_tags AS t WHERE t.tag in ({tags}))", fetch)
+	tags = tuple(map(int, tags))
+	ret = await execute(request,"SELECT * FROM toilet_status as s WHERE s.id IN (SELECT t.toilet FROM toilet_tags AS t WHERE t.tag IN %s)", fetch, params=(tags,))
 	return ret
 
 async def insert_toilet(request, lat, lng, name, poi_ID):
-	await execute(request, f"INSERT INTO toilets (lat, lng, name, poi_id) VALUES ({lat}, {lng}, '{name}', {poi_ID})")
+	await execute(request, "INSERT INTO toilets (lat, lng, name, poi_id) VALUES (%s, %s, %s, %s)", params=(lat, lng, name, poi_ID))
 
 async def insert_user(request, uname, bcrypt_hash, admin=False):
-	admin = "TRUE" if admin else "FALSE"
-	await execute(request, f"INSERT INTO users (uname, bcrypt_hash, admin) VALUES ('{uname}', '{bcrypt_hash}', {admin})")
+	admin = bool(admin)
+	await execute(request, "INSERT INTO users (uname, bcrypt_hash, admin) VALUES (%s, %s, %s)", params=(uname, bcrypt_hash, admin))
 
 async def update_user_password(request, uname, bcrypt_hash):
-	await execute(request, f"UPDATE users SET bcrypt_hash='{bcrypt_hash}' WHERE uname='{uname}'")
-
+	await execute(request, "UPDATE users SET bcrypt_hash=%s WHERE uname=%s", params=(bcrypt_hash, uname))
 async def select_user(request, uname):
-	ret = await execute(request, f"SELECT * FROM users WHERE uname = '{uname}'", 1)
+	ret = await execute(request, "SELECT * FROM users WHERE uname = %s", 1, params=(uname,))
 	return ret
